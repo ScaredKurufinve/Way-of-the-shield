4215de3fae91049aa6a90baf9660ab6c4cc91466
ScaredKurufinve <Kaymarciy@gmail.com>
2023-03-15 04:48:15 +0300

    Moved settings back? Seems to work?

--- Way of the shield/Settings.cs
+++ Way of the shield/Settings.cs
@@ -21,38 +21,65 @@ namespace Way_of_the_shield
 
 
         #region Setting Entities
-        public static SettingsEntityBool AllowEquipNonProfficientItems                   ;
-        public static SettingsEntityBool ForbidCloseFlanking                             ;
-        public static SettingsEntityBool AllowCloseFlankingToEnemies                     ;
-        public static SettingsEntityBool ConcealmentAttackBonusOnBackstab                ;
-        public static SettingsEntityBool DenyShieldBonusOnBackstab                       ;
-        public static SettingsEntityBool FlatFootedOnBackstab                            ;
-        public static SettingsEntityBool AddBucklerParry                                 ;
-        public static SettingsEntityBool AllowTwoHanded_as_OneHandedWhenBuckler          ;
-        public static SettingsEntityBool AllowBucklerBashWhenTwoHandedWithUnhindering    ;
-        public static SettingsEntityBool AllowTwoHandedSpears_as_OneHandedWhenMounted    ;
-        public static SettingsEntityBool GiveImmediateRepositioningToTSS                 ;
-        public static SettingsEntityBool RemoveTotalCoverFeatureFromTSS                  ;
-        public static SettingsEntityBool AllowShieldBashToAllWhoProficient               ;
-        public static SettingsEntityBool EnableSoftCover                                 ;
-        public static SettingsEntityBool ChangeBackToBack                                ;
-        public static SettingsEntityBool AddSoftCoverDenialToImprovedPreciseShot         ;
-        public static SettingsEntityBool ChangeShieldWall                                ;
-        public static SettingsEntityBool BuffSacredShieldEnhacementArray                 ;
-        public static SettingsEntityBool ChangeShieldSpell                               ;
-        public static SettingsEntityBool RemoveBucklerProficiencies                      ;
-        public static SettingsEntityBool FixRapiers                                      ;
+        public static SettingsEntityBool AllowEquipNonProfficientItems                  = new (settingsModName + "allow-equip-non-profficient-items", true, false, true);
+        public static SettingsEntityBool ForbidCloseFlanking                            = new (settingsModName + "forbid-close-flanking", true, false, true);
+        public static SettingsEntityBool AllowCloseFlankingToEnemies                    = new(settingsModName + "allow-close-flanking-to-enemies", true, false, false);
+        public static SettingsEntityBool ConcealmentAttackBonusOnBackstab               = new(settingsModName + "concealment-attack-bonus-on-backstab", true, false, false);
+        public static SettingsEntityBool DenyShieldBonusOnBackstab                      = new (settingsModName + "deny-shield-bonus-on-backstab", true, false, false);
+        public static SettingsEntityBool FlatFootedOnBackstab                           = new (settingsModName + "flat-footed-on-backstab", true, true, true);
+        public static SettingsEntityBool AddBucklerParry                                = new (settingsModName + "add-buckler-parry", true, false, true);
+        public static SettingsEntityBool AllowTwoHanded_as_OneHandedWhenBuckler         = new (settingsModName + "allow-two-handed-as-one-handed-when-buckler", true, false, true);
+        public static SettingsEntityBool AllowBucklerBashWhenTwoHandedWithUnhindering   = new (settingsModName + "allow-buckler-bash-when-two-handed-with-unhindering", false, false, false);
+        public static SettingsEntityBool AllowTwoHandedSpears_as_OneHandedWhenMounted   = new (settingsModName + "allow-two-handed-spears-as-one-handed-when-mounted", true, false, true);
+        public static SettingsEntityBool GiveImmediateRepositioningToTSS                = new (settingsModName + "give-immediate-repositioning-to-tss", true, false, true);
+        public static SettingsEntityBool RemoveTotalCoverFeatureFromTSS                 = new (settingsModName + "remove-total-cover-feature-from-tss", true, false, true);
+        public static SettingsEntityBool AllowShieldBashToAllWhoProficient              = new (settingsModName + "allow-shield-bash-without-improved-feature", true, false, true);
+        public static SettingsEntityBool EnableSoftCover                                = new (settingsModName + "enable-soft-sover", true, false, true);
+        public static SettingsEntityBool ChangeBackToBack                               = new (settingsModName + "change-back-to-back", true, false, true);
+        public static SettingsEntityBool AddSoftCoverDenialToImprovedPreciseShot        = new (settingsModName + "add-soft-cover-denial-to-improved-precise-shot", true, false, true);
+        public static SettingsEntityBool ChangeShieldWall                               = new (settingsModName + "change-shield-wall", true, false, true);
+        public static SettingsEntityBool BuffSacredShieldEnhacementArray                = new (settingsModName + "buff-sacred-shield-enhacement-array", true, false, true);
+        public static SettingsEntityBool ChangeShieldSpell                              = new (settingsModName + "change-shield-spell", true, false, true);
+        public static SettingsEntityBool RemoveBucklerProficiencies                     = new (settingsModName + "remove-buckler-proficiencies", true, false, true);
+        public static SettingsEntityBool FixRapiers                                     = new (settingsModName + "fix-rapiers", true, true, false);
 #if DEBUG
         public static SettingsEntityBool Debug = new(settingsModName + "debug", false); 
 #endif
         #endregion
         static UISettingsGroup SettingsGroup = ScriptableObject.CreateInstance<UISettingsGroup>();
-        static internal List<(SettingsEntityBool, UISettingsEntityBool)> ListOfBoolSettings;
+        static internal List<(SettingsEntityBool, UISettingsEntityBool)> ListOfBoolSettings = new()
+            {
+                 new(AllowEquipNonProfficientItems, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
+                 new(ForbidCloseFlanking, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AllowCloseFlankingToEnemies, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(ConcealmentAttackBonusOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(FlatFootedOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(DenyShieldBonusOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(ChangeBackToBack, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AddBucklerParry, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AllowTwoHanded_as_OneHandedWhenBuckler, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AllowBucklerBashWhenTwoHandedWithUnhindering, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AllowTwoHandedSpears_as_OneHandedWhenMounted, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(GiveImmediateRepositioningToTSS, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(RemoveTotalCoverFeatureFromTSS, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AllowShieldBashToAllWhoProficient, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(EnableSoftCover, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(AddSoftCoverDenialToImprovedPreciseShot, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(ChangeShieldWall, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(BuffSacredShieldEnhacementArray, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
+                 new(ChangeShieldSpell, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
+                 new(RemoveBucklerProficiencies, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
+                 new(FixRapiers, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
+
+                 #if DEBUG
+                 new(Debug, ScriptableObject.CreateInstance<UISettingsEntityBool>()),  
+                 #endif
+        
+            };
 
 
         static public void Init()
         {
-            DoInit();
             AddSettingsNames();
             List<UISettingsEntityBase> l = new();
             foreach (var (entity, visual) in ListOfBoolSettings) { visual.LinkSetting(entity); l.Add(visual); };
@@ -112,60 +139,6 @@ namespace Way_of_the_shield
             initialized = true;
         }
 
-        static void DoInit()
-        {
-            AllowEquipNonProfficientItems = new(settingsModName + "allow-equip-non-profficient-items", true, false, true);
-            ForbidCloseFlanking = new(settingsModName + "forbid-close-flanking", true, false, true);
-            AllowCloseFlankingToEnemies = new(settingsModName + "allow-close-flanking-to-enemies", true, false, false);
-            ConcealmentAttackBonusOnBackstab = new(settingsModName + "concealment-attack-bonus-on-backstab", true, false, false);
-            DenyShieldBonusOnBackstab = new(settingsModName + "deny-shield-bonus-on-backstab", true, false, false);
-            FlatFootedOnBackstab = new(settingsModName + "flat-footed-on-backstab", true, true, true);
-            AddBucklerParry = new(settingsModName + "add-buckler-parry", true, false, true);
-            AllowTwoHanded_as_OneHandedWhenBuckler = new(settingsModName + "allow-two-handed-as-one-handed-when-buckler", true, false, true);
-            AllowBucklerBashWhenTwoHandedWithUnhindering = new(settingsModName + "allow-buckler-bash-when-two-handed-with-unhindering", false, false, false);
-            AllowTwoHandedSpears_as_OneHandedWhenMounted = new(settingsModName + "allow-two-handed-spears-as-one-handed-when-mounted", true, false, true);
-            GiveImmediateRepositioningToTSS = new(settingsModName + "give-immediate-repositioning-to-tss", true, false, true);
-            RemoveTotalCoverFeatureFromTSS = new(settingsModName + "remove-total-cover-feature-from-tss", true, false, true);
-            AllowShieldBashToAllWhoProficient = new(settingsModName + "allow-shield-bash-without-improved-feature", true, false, true);
-            EnableSoftCover = new(settingsModName + "enable-soft-sover", true, false, true);
-            ChangeBackToBack = new(settingsModName + "change-back-to-back", true, false, true);
-            AddSoftCoverDenialToImprovedPreciseShot = new(settingsModName + "add-soft-cover-denial-to-improved-precise-shot", true, false, true);
-            ChangeShieldWall = new(settingsModName + "change-shield-wall", true, false, true);
-            BuffSacredShieldEnhacementArray = new(settingsModName + "buff-sacred-shield-enhacement-array", true, false, true);
-            ChangeShieldSpell = new(settingsModName + "change-shield-spell", true, false, true);
-            RemoveBucklerProficiencies = new(settingsModName + "remove-buckler-proficiencies", true, false, true);
-            FixRapiers = new(settingsModName + "fix-rapiers", true, true, false);
-
-            ListOfBoolSettings = new()
-            {
-                 new(AllowEquipNonProfficientItems, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
-                 new(ForbidCloseFlanking, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AllowCloseFlankingToEnemies, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(ConcealmentAttackBonusOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(FlatFootedOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(DenyShieldBonusOnBackstab, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(ChangeBackToBack, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AddBucklerParry, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AllowTwoHanded_as_OneHandedWhenBuckler, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AllowBucklerBashWhenTwoHandedWithUnhindering, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AllowTwoHandedSpears_as_OneHandedWhenMounted, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(GiveImmediateRepositioningToTSS, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(RemoveTotalCoverFeatureFromTSS, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AllowShieldBashToAllWhoProficient, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(EnableSoftCover, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(AddSoftCoverDenialToImprovedPreciseShot, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(ChangeShieldWall, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(BuffSacredShieldEnhacementArray, ScriptableObject.CreateInstance < UISettingsEntityBool >()),
-                 new(ChangeShieldSpell, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
-                 new(RemoveBucklerProficiencies, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
-                 new(FixRapiers, ScriptableObject.CreateInstance<UISettingsEntityBool>()),
-
-                 #if DEBUG
-                 new(Debug, ScriptableObject.CreateInstance<UISettingsEntityBool>()),  
-                 #endif
-        
-            };
-        }
 
         static public void Enable(SettingsEntityBool SettingName)
         {
